a1634a415e5d8e25a76758a1946ab1ce46b15018
cstlford <cstlford@gmail.com>
2023-12-14 14:49:22 -0500

    Edits

--- src/pages/dashboard/quiz/quizzes/index.tsx
+++ src/pages/dashboard/quiz/quizzes/index.tsx
@@ -1,11 +1,20 @@
 import Layout from "@/src/layouts/dashboard/layout";
+import { FaSearch } from "react-icons/fa";
 
 export default function Quizzes() {
-    return (
-        <Layout>
-        <div>
-            <h1>Quizzes</h1>
+  return (
+    <Layout>
+      <div className="text-white mr-20 ml-8">
+        <h1 className="text-lg font-semibold">Quizzes</h1>
+        <div className="mt-10  flex items-center border-divider border-2 bg-transparent rounded-sm">
+          <FaSearch className="text-white mx-2" />
+          <input
+            className="w-full pl-2 py-2 text-white bg-transparent outline-none"
+            placeholder="Search for a textbook..."
+            type="text"
+          />
         </div>
-        </Layout>
-    );
+      </div>
+    </Layout>
+  );
 }
